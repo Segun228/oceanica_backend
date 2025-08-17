261d67de520bde45fd2dad28a18fb9dae35341cf
Segun228 <segunperkele@gmail.com>
2025-08-17 18:13:46 +0300

    fixing permission

--- backend/photos/permissions.py
+++ backend/photos/permissions.py
@@ -1,36 +1,53 @@
 import os
-from rest_framework import permissions
+import logging
 from dotenv import load_dotenv
+from django.contrib.auth import get_user_model
+from rest_framework import permissions
 
 load_dotenv()
 
-admins = (os.getenv("ADMINS"))
-if not admins:
+
+admins_raw = os.getenv("ADMINS")
+if not admins_raw:
     raise ValueError("Empty admin list given")
-else:
-    admins = list(map(str.strip, admins.split("_")))
+admins = set(map(str.strip, admins_raw.split("_")))
+
 
 debug = os.getenv("DEBUG", "False").lower() == "true"
 
 
 class PhotoIsAdminOrDebugOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        User = get_user_model()
+
+
         if request.method in permissions.SAFE_METHODS:
             return True
+
 
         if request.method.upper() != "PATCH":
             return False
 
+
         auth = request.headers.get("Authorization")
         if not auth:
             return False
+
         parts = auth.split(" ")
         if len(parts) != 2 or parts[0] != "Bot":
             return False
+
         token = parts[1]
 
-        if token not in admins and not debug:
+        try:
+            user = User.objects.get(telegram_id=token)
+        except User.DoesNotExist:
+            logging.warning(f"Пользователь с Telegram ID {token} не найден.")
             return False
+
+
+        if (user.is_staff and str(user.telegram_id) in admins):
+            return True
 
         allowed_fields = {"photos"}
         incoming_fields = set(request.data.keys())
